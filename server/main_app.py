35bd63b5486bf34fb0099f75a76fc3eb2e3c6ddf
cr-ziji <cr-ziji@users.noreply.github.com>
2025-04-28 20:53:29 +0800

    Update main_app.py

--- server/main_app.py
+++ server/main_app.py
@@ -46,34 +46,50 @@ def register():
 def handle():
     if request.form['type'] == '登录':
         dict1 = {
-            '用户名': request.form['nickname'],
+            '手机号': request.form['tel'],
             '密码': request.form['password']
         }
         have = db.teacher.find_one(dict1)
         if have is None:
             return redirect('/login?alert=1')
         session['用户名'] = have['用户名']
+        session['手机号'] = have['手机号']
         session['密码'] = have['密码']
+        session['班级'] = have['班级']
         return redirect('/home')
     elif request.form['type'] == '注册':
+        name = request.form['subject']+request.form['tel']+'老师'
         dict1 = {
-            '用户名': request.form['nickname'],
-            '密码': request.form['password']
+            '用户名': name,
+            '手机号': request.form['tel'],
+            '密码': request.form['password'],
+            '班级': dict(request.form)['class']
         }
-        have = db.teacher.find_one({'用户名': request.form['nickname']})
+        have = db.teacher.find_one({'用户名': name})
         if have is not None:
             return redirect('/register?alert=1')
         db.teacher.insert_one(dict1)
-        session['用户名'] = request.form['nickname']
+        session['用户名'] = name
+        session['手机号'] = request.form['tel']
         session['密码'] = request.form['password']
+        session['班级'] = dict(request.form)['class']
         return redirect('/home')
 
 
 @app.route('/home')
 def home():
-    t = time.strftime("%Y-%m", time.localtime())
     return render_template('home.html',
-                           t_nickname=session['昵称'])
+                           t_name=session['用户名'],
+                           t_class=session['班级'])
+
+
+@app.route('/class')
+def class1():
+    if request.args['class'] not in session['班级']:
+        return redirect('/home')
+    data = db.data.find_one({'班级': request.args['class']})
+    return render_template('home.html',
+                           t_data=data)
 
 if __name__ == '__main__':
     app.run(port=5001, debug=True)

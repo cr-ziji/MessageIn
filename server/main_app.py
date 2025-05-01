981cdcad33f9d6ba8884bda56a4db512bb7b87e7
cyrilguocode <cyril_0614@hotmail.com>
2025-05-01 13:09:43 +0800

    server-5/1

--- server/main_app.py
+++ server/main_app.py
@@ -65,9 +65,9 @@ def handle():
             '密码': request.form['password'],
             '班级': dict(request.form)['class']
         }
-        have = db.teacher.find_one({'用户名': name})
-        if have is not None:
-            return redirect('/register?alert=1')
+        # have = db.teacher.find_one({'用户名': name})
+        # if have is not None:
+        #     return redirect('/register?alert=1')
         db.teacher.insert_one(dict1)
         session['用户名'] = name
         session['手机号'] = request.form['tel']
@@ -80,16 +80,34 @@ def handle():
 def home():
     return render_template('home.html',
                            t_name=session['用户名'],
-                           t_class=session['班级'])
+                           t_class=session['班级'],
+                           t_range=range(len(session['班级'])))
 
 
 @app.route('/class')
 def class1():
     if request.args['class'] not in session['班级']:
         return redirect('/home')
-    data = db.data.find_one({'班级': request.args['class']})
-    return render_template('home.html',
-                           t_data=data)
+    data = db.data.find_one({'class': request.args['class']})['message']
+    return render_template('class.html',
+                           t_data=data,
+                           t_name=session['用户名'],
+                           t_class=request.args['class'])
+
+@app.route('/send')
+def send():
+    data = db.data.find_one({'class': request.args['class']})
+    if data is None:
+        data = {'class': request.args['class'], 'message':[]}
+        db.data.insert_one(data)
+    data['message'].append({
+        'name': session['用户名'],
+        'content': request.args['content'],
+        'time': 'aaaa',
+        'isread': False,
+    })
+    db.data.update_one({'class': request.args['class']}, {'$set': data})
+    return ''
 
 if __name__ == '__main__':
     app.run(port=5001, debug=True)

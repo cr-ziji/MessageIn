fef8b3997ea1835019bd8cec1dfc061e7baffe94
cyrilguocode <cyril_0614@hotmail.com>
2025-05-10 14:59:31 +0800

    server-3.0.1

--- server/main_app.py
+++ server/main_app.py
@@ -1,13 +1,15 @@
 # coding=UTF-8
 from flask import Flask, request, render_template, redirect, session
+from flask_socketio import SocketIO, emit, join_room, leave_room
 import pymongo
-from pyttsx3 import init
-import uuid, time, random
+import uuid, time
 
 client = pymongo.MongoClient()
 db = client.main
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'  # 加密密钥（生产环境需修改）
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet', logger=True, engineio_logger=True)  # 允许跨域
 
 app.secret_key = '123456'
 classlist = {
@@ -20,12 +22,6 @@ classlist = {
 }
 
 
-@app.after_request
-def add_cors_headers(response):
-    response.headers['Access-Control-Allow-Origin'] = '*'  # 允许所有域名
-    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
-    response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
-    return response
 
 @app.route('/login')
 def login():
@@ -99,6 +95,7 @@ def home():
 
 @app.route('/class')
 def class1():
+    print(session)
     if request.args['class'] not in session['班级']:
         return redirect('/home')
     data = db.data.find_one({'class': request.args['class']})
@@ -111,130 +108,155 @@ def class1():
                            t_name=session['用户名'],
                            t_class=request.args['class'])
 
-@app.route('/send')
-def send():
-    uuid1 = str(uuid.uuid1())
-    class1 = request.args['class']
-    time1 = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+@socketio.on('connect')
+def handle_connect():
+    print('客户端已连接')
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    print('客户端断开连接')
+
+@socketio.on('send')
+def send(data):
     isread = '未读'
+    uuid1 = str(uuid.uuid1())
+    class1 = data['class']
+    content = data['content']
+    time1 = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
     if '通知' not in class1:
-        data_send(class1,uuid1,time1, '')
+        data_send(content, class1,uuid1,time1, '')
     elif class1 != '全校通知':
         isread += ' : '
         l = classlist[class1[0:2]]
-        data = db.data.find_one({'class': request.args['class']})
-        data['message'].append({
+        data1 = db.data.find_one({'class': data['class']})
+        data1['message'].append({
             'uuid': uuid1,
             'name': session['用户名'],
-            'content': request.args['content'],
+            'content': data['content'],
             'time': time1,
             'isread': [],
             'type': ''
         })
         for i in l:
-            data_send(class1[0:2]+i, uuid1, time1, class1)
-            data['message'][-1]['isread'].append(class1[0:2]+i)
+            data_send(content, class1[0:2]+i, uuid1, time1, class1)
+            data1['message'][-1]['isread'].append(class1[0:2]+i)
             isread += class1[0:2]+i + ' '
-        db.data.update_one({'class': request.args['class']}, {'$set': data})
+        db.data.update_one({'class': class1}, {'$set': data1})
+        data1['message'][-1]['class1'] = class1
+        data1['message'][-1]['isread'] = data_isread(data1['message'][-1]['isread'])
+        emit('new', data1['message'][-1], class1=class1, broadcast=True)
     else:
         isread += ' : '
-        data = db.data.find_one({'class': request.args['class']})
-        data['message'].append({
+        data1 = db.data.find_one({'class': data['class']})
+        data1['message'].append({
             'uuid': uuid1,
             'name': session['用户名'],
-            'content': request.args['content'],
+            'content': data['content'],
             'time': time1,
             'isread': [],
             'type': ''
         })
         for i in classlist:
             for j in classlist[i]:
-                data_send(i+j, uuid1, time1, class1)
-                data['message'][-1]['isread'].append(i+j)
+                data_send(content, i+j, uuid1, time1, class1)
+                data1['message'][-1]['isread'].append(i+j)
                 isread += i+j + ' '
-        db.data.update_one({'class': request.args['class']}, {'$set': data})
-    return '{"uuid":"' + uuid1 + '", "time":"' + time1 + '", "isread":"' + isread + '"}'
+        db.data.update_one({'class': data['class']}, {'$set': data1})
+        data1['message'][-1]['class1'] = class1
+        data1['message'][-1]['isread'] = data_isread(data1['message'][-1]['isread'])
+        emit('new', data1['message'][-1], class1=class1, broadcast=True)
 
-@app.route('/back')
-def back():
-    uuid1 = request.args['uuid']
-    class1 = request.args['class']
+@socketio.on('back_data')
+def back(data):
+    uuid1 = data['uuid']
+    class1 = data['class']
     data_back(class1, uuid1)
     if '通知' in class1 and class1 != '全校通知':
         l = classlist[class1[0:2]]
         for i in l:
             data_back(class1[0:2]+i, uuid1)
-    else:
+    elif class1 == '全校通知':
         for i in classlist:
             for j in classlist[i]:
                 data_back(i+j, uuid1)
-    return ''
 
-@app.route('/message')
-def message():
-    class1 = request.args['class']
-    data = db.data.find_one({'class': class1})
-    if data is None:
-        return ''
-    if data['message'][-1]['content'] != '此消息已撤回':
-        content = data['message'][-1]['name'] + ':' + data['message'][-1]['content']
-    else:
-        content = '此消息已撤回'
-    return '{"new":{"uuid":"' + data['message'][-1]['uuid'] + '","content":"' + content + '"},"back":{"uuid":"' + data['back'] + '"}}'
+# @app.route('/message')
+# def message():
+#     class1 = request.args['class']
+#     data = db.data.find_one({'class': class1})
+#     if data is None:
+#         return ''
+#     if data['message'][-1]['content'] != '此消息已撤回':
+#         content = data['message'][-1]['name'] + ':' + data['message'][-1]['content']
+#     else:
+#         content = '此消息已撤回'
+#     return '{"new":{"uuid":"' + data['message'][-1]['uuid'] + '","content":"' + content + '"},"back":{"uuid":"' + data['back'] + '"}}'
 
-@app.route('/update')
-def update():
-    class1 = request.args['class']
-    data = db.data.find_one({'class': class1})
-    if data is None:
+@socketio.on('isread')
+def isread(data):
+    class1 = data['class']
+    data1 = db.data.find_one({'class': class1})
+    if data1 is None:
         return ''
-    l = data['message']
+    l = data1['message']
     for i in range(len(l)):
-        if l[i]['uuid'] == request.args['uuid']:
-            data['message'][i]['isread'] = True
-            if data['message'][i]['type'] != '':
-                data1 = db.data.find_one({'class': data['message'][i]['type']})
-                l1 = data1['message']
+        if l[i]['uuid'] == data['uuid']:
+            data1['message'][i]['isread'] = True
+            db.data.update_one({'class': class1}, {'$set': data1})
+
+            emit('update', {'uuid': data['uuid'], 'isread': '已读', 'class1': class1}, class1=class1, broadcast=True)
+            if data1['message'][i]['type'] != '':
+                data2 = db.data.find_one({'class': data1['message'][i]['type']})
+                l1 = data2['message']
                 for j in range(len(l1)):
-                    if l1[j]['uuid'] == request.args['uuid']:
-                        if class1 in data1['message'][j]['isread']:
-                            data1['message'][j]['isread'].remove(class1)
-                            db.data.update_one({'class': data['message'][i]['type']}, {'$set': data1})
+                    if l1[j]['uuid'] == data['uuid']:
+                        if class1 in data2['message'][j]['isread']:
+                            data2['message'][j]['isread'].remove(class1)
+                            db.data.update_one({'class': data2['message'][i]['type']}, {'$set': data1})
+                            emit('update', {'uuid': data['uuid'], 'isread': data_isread(data2['message'][i]['isread']), 'class1': data1['message'][i]['type']}, class1=data1['message'][i]['type'], broadcast=True)
                         break
             break
-    db.data.update_one({'class': request.args['class']}, {'$set': data})
-    return ''
 
-def data_send(class1, uuid1, time1, type1):
-    data = db.data.find_one({'class': class1})
-    data['message'].append({
+def data_send(content, class1, uuid1, time1, type1):
+    data1 = db.data.find_one({'class': class1})
+    data1['message'].append({
         'uuid': uuid1,
         'name': session['用户名'],
-        'content': request.args['content'],
+        'content': content,
         'time': time1,
         'isread': False,
         'type': type1
     })
-    db.data.update_one({'class': class1}, {'$set': data})
+    db.data.update_one({'class': class1}, {'$set': data1})
+    data1['message'][-1]['class1'] = class1
+    data1['message'][-1]['isread'] = '未读'
+    emit('new', data1['message'][-1], class1=class1, broadcast=True)
 
 def data_back(class1, uuid1):
-    data = db.data.find_one({'class': class1})
-    l = data['message']
+    data1 = db.data.find_one({'class': class1})
+    l = data1['message']
     for i in range(len(l)):
         if l[i]['uuid'] == uuid1:
-            data['message'][i]['content'] = '此消息已撤回'
+            data1['message'][i]['content'] = '此消息已撤回'
             break
-    data['back'] = uuid1
-    db.data.update_one({'class': class1}, {'$set': data})
+    data1['back'] = uuid1
+    db.data.update_one({'class': class1}, {'$set': data1})
+    emit('back', {'uuid': uuid1, 'class1': class1}, class1=class1, broadcast=True)
+
+def data_isread(l):
+    if l == []:
+        return '已读'
+    else:
+        return '未读 : ' + ' '.join(l)
 
 @app.errorhandler(404)
 def error_date_404(error):
     return redirect('/login')
 
-# @app.errorhandler(Exception)
-# def error_date_500(error):
-#     return 'error code:500'
+@app.errorhandler(Exception)
+def error_date_500(error):
+    return 'error code:500'
 
 
 if __name__ == '__main__':
-    app.run(port=5001, debug=True)
+    socketio.run(app, host='127.0.0.1', port=5001, debug=True)
